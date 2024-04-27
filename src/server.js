1f952b3908bbd178f71aa3b169d79378286f2fdd
WenerRodrigo <wener19.wr@gmail.com>
2024-04-26 21:46:36 -0300

    chore: validate token with axios

--- src/server.js
+++ src/server.js
@@ -5,10 +5,35 @@ if (process.env.NODE_ENV !== "production") {
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
+const https = require("https");
 
-const cert = fs.readFileSync(path.resolve(__dirname, "../certs/cert.pem"));
+const cert = fs.readFileSync(
+  path.resolve(__dirname, `../certs/${process.env.GN_CERT}`)
+);
 
-console.log(process.env);
+const agent = new https.Agent({
+  pfx: cert,
+  passphrase: "",
+});
+
+const credentials = Buffer.from(
+  `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
+).toString("base64");
+
+axios({
+  method: "POST",
+  url: `${process.env.GN_ENDPOINT}/oauth/token`,
+  headers: {
+    Authorization: `Basic ${credentials}`,
+    "Content-Type": "application/json",
+  },
+  httpsAgent: agent,
+  data: {
+    grant_type: "client_credentials",
+  },
+}).then((response) => {
+  console.log(response.data);
+});
 
 /*
     curl --request POST \
